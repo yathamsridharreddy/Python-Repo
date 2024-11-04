d3c8dc13648bb34ae491ce3fd12ec0886e254610
yathamsridharreddy <147792360+yathamsridharreddy@users.noreply.github.com>
2024-11-04 11:06:46 +0530

    Uploading code: Instant Noodles.py

---
+++ Instant Noodles.py
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class InstantNoodles
+{
+    public static void main(String args[])
+    {
+ Scanner sc=new Scanner(System.in);
+ int a=sc.nextInt();
+ int b=sc.nextInt();
+ int max=a*b;
+ System.out.println(max);
+    }
+}
