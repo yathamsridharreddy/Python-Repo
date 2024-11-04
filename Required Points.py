5865d53f2ba0aaf24b5626fac44f0931a751dd9d
yathamsridharreddy <147792360+yathamsridharreddy@users.noreply.github.com>
2024-11-04 10:45:01 +0530

    Uploading code: Required Points.py

---
+++ Required Points.py
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class RequiredPoints
+{
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int result=b/a;
+      System.out.println(result);
+    }
+}
