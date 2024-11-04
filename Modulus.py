b91c10a4d06b748487f31f590b205bbbcf5c065c
yathamsridharreddy <147792360+yathamsridharreddy@users.noreply.github.com>
2024-11-04 11:48:25 +0530

    Uploading code: Modulus.py

---
+++ Modulus.py
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Modulus {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        int a = sc.nextInt();
+
+        
+        int b = sc.nextInt();
+        int m=a%b;
+        System.out.println(m);
+    }
+}
